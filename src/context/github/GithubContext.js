64bcc0ba83356791deb45edb7476e25fa3044791
Jeff Derbes <derbes.jeffrey@gmail.com>
2022-04-23 07:43:41 -0500

    initial commit

---
+++ src/context/github/GithubContext.js
@@ -0,0 +1,32 @@
+import { createContext } from "react";
+import { useReducer } from "react";
+import githubReducer from "./GithubReducer";
+
+const GithubContext = createContext();
+
+const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+
+export const GithubProvider = ({ children }) => {
+  const initalState = {
+    users: [],
+    loading: false,
+    repos: [],
+    user: {},
+  };
+
+  const [state, dispatch] = useReducer(githubReducer, initalState);
+
+  return (
+    <GithubContext.Provider
+      value={{
+        ...state,
+        dispatch,
+      }}
+    >
+      {children}
+    </GithubContext.Provider>
+  );
+};
+
+export default GithubContext;
